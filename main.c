380aa2fe70f72af8a2f8362ec1f32148018f2950
Muslim ALiev <muslimaliev@localhost.localdomain>
2024-06-05 13:59:00 +0200

    patrick

--- main.c
+++ main.c
@@ -31,15 +31,22 @@ struct Vertex vertices[VAL];
 struct TexCoord uvs[VAL];
 struct Normal normals[VAL];
 int vertNum = 0, uvNum = 0, normNum = 0;
-struct Vertex Patrick_vertices[VAL];
-struct TexCoord Patrick_uvs[VAL];
-struct Normal Patrick_normals[VAL];
-int Patrick_vertNum = 0, Patrick_uvNum = 0, Patrick_normNum = 0;
+struct Vertex patrick_vertices[VAL];
+struct TexCoord patrick_uvs[VAL];
+struct Normal patrick_normals[VAL];
+int patrick_vertNum = 0, patrick_uvNum = 0, patrick_normNum = 0;
 
-GLuint program;
+
+GLfloat transMat[16];
+GLfloat viewMat[16];
+GLfloat projMat[16];
+GLfloat patrick_transMat[16];
+GLfloat patrick_viewMat[16];
+GLfloat patrick_projMat[16];
+GLuint program, patrick_program;
 GLuint sbProgram;
 GLuint vao;
-GLuint skyboxVAO , PatrickVAO;
+GLuint skyboxVAO , patrickVAO;
 unsigned int cubemapTexture;
 GLfloat dir = 1;
 GLfloat xScale = 0.1f;
@@ -73,7 +80,7 @@ GLfloat input[] = {
 GLfloat testvektor[] = {
     0.0f,
     0.0f,
-    1.0f
+    0.0f
     
 };
 GLfloat scaler[] = {
@@ -158,6 +165,12 @@ int loadOBJ(
         out_normals[*normNum] = normal;
         *normNum +=1;
     }
+    for (unsigned int i = 0; i < uvCount; i++) {
+        unsigned int uvIndex = uvIndices[i];
+        struct TexCoord uv = temp_uvs[uvIndex-1];
+        out_uvs[*uvNum] = uv;
+        *uvNum +=1;
+    }
     return 0;
 }
 
@@ -174,7 +187,7 @@ void init(void) {
         printf("Error compiling vertex shader:");
         GLchar infoLog[1024];
         glGetShaderInfoLog(vertexShader, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
     }
 
     // create and compile vertex shader
@@ -188,7 +201,7 @@ void init(void) {
         printf("Error compiling fragment shader:");
         GLchar infoLog[1024];
         glGetShaderInfoLog(fragmentShader, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
     }
     // create and link shader program
     program = glCreateProgram();
@@ -200,7 +213,7 @@ void init(void) {
         printf("Error linking program:");
         GLchar infoLog[1024];
         glGetProgramInfoLog(program, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
     }
     glValidateProgram(program);
     glGetProgramiv(program, GL_VALIDATE_STATUS, &status);
@@ -208,7 +221,27 @@ void init(void) {
         printf("Error linking program:");
         GLchar infoLog[1024];
         glGetProgramInfoLog(program, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
+    }
+    //patrick program
+    patrick_program = glCreateProgram();
+    glAttachShader(patrick_program, vertexShader);
+    glAttachShader(patrick_program, fragmentShader);
+    glLinkProgram(patrick_program);
+    glGetProgramiv(patrick_program, GL_LINK_STATUS, &status);
+    if(!status) {
+        printf("Error linking patrick program:");
+        GLchar infoLog[1024];
+        glGetProgramInfoLog(patrick_program, 1024, NULL, infoLog);
+        printf("%s",infoLog);
+    }
+    glValidateProgram(patrick_program);
+    glGetProgramiv(patrick_program, GL_VALIDATE_STATUS, &status);
+    if(!status) {
+        printf("Error linking patrick program:");
+        GLchar infoLog[1024];
+        glGetProgramInfoLog(patrick_program, 1024, NULL, infoLog);
+        printf("%s",infoLog);
     }
 
     //skybox shader
@@ -223,7 +256,7 @@ void init(void) {
         printf("Error compiling vertex shader:");
         GLchar infoLog[1024];
         glGetShaderInfoLog(vertexShadersb, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
     }
 
     // create and compile vertex shader
@@ -237,7 +270,7 @@ void init(void) {
         printf("Error compiling fragment shader:");
         GLchar infoLog[1024];
         glGetShaderInfoLog(fragmentShadersb, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
     }
     // create and link shader program
     sbProgram = glCreateProgram();
@@ -249,7 +282,7 @@ void init(void) {
         printf("Error linking program skybox:");
         GLchar infoLog[1024];
         glGetProgramInfoLog(sbProgram, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
     }
     glValidateProgram(sbProgram);
     glGetProgramiv(sbProgram, GL_VALIDATE_STATUS, &statussb);
@@ -257,15 +290,15 @@ void init(void) {
         printf("Error linking program:");
         GLchar infoLog[1024];
         glGetProgramInfoLog(sbProgram, 1024, NULL, infoLog);
-        printf(infoLog);
+        printf("%s",infoLog);
     }
 
 
    
     
     int res = loadOBJ("spongebobs_pineapple_house.obj", vertices, uvs, normals, &vertNum, &uvNum, &normNum);
-    GLfloat norms[3*normNum];
-    int count = 0;
+    if(res!=0)
+        perror("failed to load pineapple");
     
     GLuint vbo;
     glGenBuffers(1, &vbo);
@@ -324,41 +357,57 @@ void init(void) {
     glBindVertexArray(0);
     //patrick TODO1111
 
-    /*int res = loadOBJ("patrick_star_spongebob.obj", vertices, uvs, normals, &vertNum, &uvNum, &normNum);
-    GLfloat norms[3*normNum];
-    int count = 0;
+    int patrick = loadOBJ("patrick.obj", patrick_vertices, patrick_uvs, patrick_normals, &patrick_vertNum, &patrick_uvNum, &patrick_normNum);
+    if(patrick !=0)
+        perror("failed to load patrick");
     
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, vertNum * sizeof(struct Vertex), vertices, GL_STATIC_DRAW);
+    
+    GLuint patrickVBO;
+    glGenBuffers(1, &patrickVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, patrickVBO);
+    glBufferData(GL_ARRAY_BUFFER, patrick_vertNum * sizeof(struct Vertex), patrick_vertices, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     
     
-    GLuint norm;
-    glGenBuffers(1, &norm);
-    glBindBuffer(GL_ARRAY_BUFFER, norm);
-    glBufferData(GL_ARRAY_BUFFER,   normNum * sizeof(struct Normal), normals, GL_STATIC_DRAW);
+    GLuint patrickNormals;
+    glGenBuffers(1, &patrickNormals);
+    glBindBuffer(GL_ARRAY_BUFFER, patrickNormals);
+    glBufferData(GL_ARRAY_BUFFER,   patrick_normNum * sizeof(struct Normal), patrick_normals, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // create vertex array object
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);*/
+    glGenVertexArrays(1, &patrickVAO);
+    glBindVertexArray(patrickVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, patrickVBO);
+    glVertexAttribPointer(
+        0, 
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        0,
+        0
+    );
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, patrickNormals);
+    glVertexAttribPointer(
+        1, 
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        0,
+        0
+    );
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
 
     //skybox
 
     GLfloat skyBoxVertices[] = {
 
-        /*-1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
-        1.0f, -1.0f, 1.0f,  1.0f, 0.0f,
-        1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-        -1.0f, -1.0f, 1.0f, 0.0f,0.0f,
-        -1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-        1.0f, 1.0f, -1.0f,  0.0f, 1.0f,
-        -1.0f, 1.0f, -1.0f,  1.0f, 1.0f*/
+       
         -1.0f,1.0f,1.0f, 0.0f, 1.0f,
         -1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
         1.0f,1.0f,1.0f, 1.0f, 1.0f,
@@ -370,24 +419,7 @@ void init(void) {
     };
 
     unsigned int skyBoxIndices[] = {
-        /*//right
-        1,2,6,
-        6,5,1,
-        //left
-        0,4,7,
-        7,3,0,
-        //top
-        4,5,6,
-        6,7,4,
-        //bottom
-        0,3,2,
-        2,1,0,
-        //back
-        0,1,5,
-        5,4,0,
-        //front
-        3,7,6,
-        6,2,3*/
+       
         0, 2, 3, 0, 3, 1,
         2, 6, 7, 2, 7, 3,
         6, 4, 5, 6, 5, 7,
@@ -493,7 +525,8 @@ void translate(GLfloat* out, GLfloat* in, GLfloat* v) {
     {
         for (int j = 0; j < 4; j++)
         {
-             out[i*4+j] = T[i*4] * in[j*4] +  T[i*4+1] * in[j*4+1] + T[i*4+2] * in[j*4+2] + T[i*4+3] * in[j*4+3];
+             //out[i*4+j] = T[i*4] * in[j*4] +  T[i*4+1] * in[j*4+1] + T[i*4+2] * in[j*4+2] + T[i*4+3] * in[j*4+3];
+            out[i*4+j] = T[i*4] * in[j*4] +  T[i*4+1] * in[j*4+1] + T[i*4+2] * in[j*4+2] + T[i*4+3] * in[j*4+3];
              
         }
         
@@ -679,9 +712,7 @@ void perspective(GLfloat* out, GLfloat fovy, GLfloat aspect, GLfloat near, GLflo
 
 void draw(void) {
     glClear(GL_COLOR_BUFFER_BIT);
-    GLfloat transMat[16];
-    GLfloat viewMat[16];
-    GLfloat projMat[16];
+    
     identity(transMat);
     identity(viewMat);
     lookAt(viewMat, eye, look, up);
@@ -708,7 +739,7 @@ void draw(void) {
     // reihenfolge scale -> rotate -> translate !!!!!
     
     //scale(transMat, transMat, scaler);
-     //rotatez(transMat, transMat, rotAngle);
+    //rotatez(transMat, transMat, rotAngle);
     //rotatex(transMat, transMat, rotAngle);
     //rotatey(transMat,transMat,rotAngle);
     //rotatey(viewMat, viewMat, rotAngle);
@@ -750,26 +781,52 @@ void draw(void) {
     glEnable(GL_DEPTH_TEST);
     
     glDrawArrays(GL_TRIANGLES, 0,vertNum);
+    glBindVertexArray(0);
+    
+
+    glUseProgram(patrick_program);
+    identity(patrick_transMat);
+    identity(patrick_viewMat);
+    lookAt(patrick_viewMat, eye, look, up);
+    perspective(patrick_projMat,50, aspect, 0.1f, 1000.0f);
+    translate(patrick_transMat,patrick_transMat,testvektor); 
+    GLint patrick_cameraLocation = glGetUniformLocation(patrick_program, "camera");
+    glUniform3f(patrick_cameraLocation,x,y,z);
+    GLint patrick_colorLocation = glGetUniformLocation(patrick_program, "color");
+    glUniform3f(patrick_colorLocation, 1.0f, 0.24f, 0.02f);
+    GLint patrick_ModelLoc = glGetUniformLocation(patrick_program, "model");
+    glUniformMatrix4fv(patrick_ModelLoc,1,GL_FALSE, patrick_transMat);
+    GLint patrick_viewLoc = glGetUniformLocation(patrick_program, "view");
+    glUniformMatrix4fv(patrick_viewLoc,1, GL_FALSE, patrick_viewMat);
+    GLint patrick_projLoc = glGetUniformLocation(patrick_program, "projection");
+    glUniformMatrix4fv(patrick_projLoc,1, GL_FALSE, patrick_projMat);
+    GLint patrick_maLocation = glGetUniformLocation(patrick_program, "material.ambient");
+    GLint patrick_mdLocation = glGetUniformLocation(patrick_program, "material.diffuse");
+    GLint patrick_msLocation = glGetUniformLocation(patrick_program, "material.specular");
+    GLint patrick_mshineLocation = glGetUniformLocation(patrick_program, "material.shininess");
+    glUniform3f(patrick_maLocation, 1.0f, 0.5f, 0.31f);
+    glUniform3f(patrick_mdLocation, 1.0f, 0.5f, 0.31f);
+    glUniform3f(patrick_msLocation, 0.5f, 0.5f, 0.5f);
+    glUniform1f(patrick_mshineLocation, 64.0f);
+    GLint patrick_laLocation = glGetUniformLocation(patrick_program, "light.ambient");
+    GLint patrick_ldLocation = glGetUniformLocation(patrick_program, "light.diffuse");
+    GLint patrick_lsLocation = glGetUniformLocation(patrick_program, "light.specular");
+    
+    glUniform3f(patrick_laLocation, 0.5f,0.0f,0.0f);
+    glUniform3f(patrick_ldLocation, 0.2f,0.0f,0.0f);
+    glUniform3f(patrick_lsLocation, 1.0f,1.0f,1.0f);
+    glBindVertexArray(patrickVAO);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    
+    glDrawArrays(GL_TRIANGLES, 0,patrick_vertNum);
+     
+    //glScalef(50.0f,50.0f,50.0f);
     //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-    if (scaler[0] >= 10.0f)
-    {
-        dir = -1;
-    } else if (scaler[0] <= 0.0f)
-    {
-        dir = 1;
-    }
-    //eye[2] = eye[2] + 0.0001;
     
-    
-    //std::cout << look[2] << std::endl;
-    
-    
-    scaler[1] = scaler[1] + dir * 0.01f;
-   
-    //scaler[0] = scaler[0] + dir * 0.01f;
     
     if(rotAngle < 360) {
-        rotAngle += 0.1f;
+        rotAngle += 0.01f;
         
     } else {
         rotAngle = 0;
@@ -807,7 +864,16 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     else if (key == GLFW_KEY_O && action == GLFW_REPEAT)
         look[1] = look[1] +0.5f;
     else if (key == GLFW_KEY_P && action == GLFW_REPEAT)
-        look[1] = look[1] -0.5f;    
+        look[1] = look[1] -0.5f;   
+    else if (key == GLFW_KEY_UP && action == GLFW_REPEAT)
+        testvektor[1] = testvektor[1] +0.5f;
+    else if (key == GLFW_KEY_DOWN && action == GLFW_REPEAT)
+        testvektor[1] = testvektor[1] -0.5f;  
+    else if (key == GLFW_KEY_RIGHT && action == GLFW_REPEAT)
+        testvektor[2] = testvektor[2] +0.5f; 
+        
+    else if (key == GLFW_KEY_LEFT && action == GLFW_REPEAT)
+        testvektor[2] = testvektor[2] -0.5f; 
 }
 int main(void)
 {
